12ac4f30668505772929c40d7e780080ea3e78c7
unknown <xuyj37889@hs.handsome.com.cn>
2022-05-11 17:25:40 +0800

    update base case

--- stepsrcgen.py
+++ stepsrcgen.py
@@ -1,14 +1,28 @@
 from common import *
 
-def onestepgen(tag35, tag10072, tag96):
-    onestep_obj = STEP(tag35, tag10072, tag96)
-    return onestep_obj
+def case1(rdlen):
+    pf1_obj = PREFETCH(rd=True, rdlen=rdlen)
+    step_obj = STEP(tag35=b'UA3201', tag10072=b'1', tag96=pf1_obj.prefetchbytes)
+    return step_obj.stepbytes
 
+def case2():
+    tmp_list = list()
+    for i in range(1_0000, 2_0000, 342):
+        tmp_list.append(case1(i))
+    return tmp_list
 
 
 if __name__ == "__main__":
-    print('hi')
-    tag35 = b'UA3201'
-    tag10072 = b'18823'
-    tag96 = b'asd123123sad1231aasdasd'
-    osp = onestepgen(tag35, tag10072, tag96)
+    import os
+    runpath = os.path.abspath('')
+    srcpath = os.path.join(runpath, 'src')
+    if not os.path.exists(srcpath):
+        os.mkdir(srcpath)
+    stepsrc_file = os.path.join(srcpath, "stepsrc.dat")
+    f = open(stepsrc_file, "wb+")
+
+    t2 = case2()
+    for i in t2:
+        f.write(i)
+    f.close()
+    print('done')
