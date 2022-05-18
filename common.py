15b24bc06b065d8ce3c166515643327df2c0ba56
unknown <xuyj37889@hs.handsome.com.cn>
2022-05-18 18:03:49 +0800

    update fast encode codes

--- common.py
+++ common.py
@@ -1,8 +1,8 @@
 from collections import OrderedDict
 from datetime import datetime
-import random
-import csv
+import random, csv, binascii, math
 import template
+
 # ================================================================================ parameter
 steptype_tuple = (
     b'UA5801',
@@ -15,11 +15,13 @@ steptype_tuple = (
     b'UA3115'
 )
 
+
 def tplist2dict(template_list):
     tp_dict = OrderedDict()
     for tag in template_list:
         tp_dict[tag[0]] = tag[1:]
     return tp_dict
+
 
 tpdict = dict()
 tpdict[3113] = tplist2dict(template.template_3113)
@@ -30,13 +32,13 @@ tpdict[3901] = tplist2dict(template.template_3901)
 tpdict[5801] = tplist2dict(template.template_5801)
 tpdict[9002] = tplist2dict(template.template_9002)
 
-
 intrange_dict = {
     'int32': (-0x7fff_ffff, 0x7fff_ffff),
     'uint32': (0, 0xffff_ffff),
     'int64': (-0x7fff_ffff_ffff_ffff, 0x7fff_ffff_ffff_ffff),
     'uint64': (0, 0xffff_ffff_ffff_ffff)
 }
+
 
 # ================================================================================ function
 def gettag10142_fromtag35(tag35: bytes) -> bytes:
@@ -59,6 +61,64 @@ def gettag10142_fromtag35(tag35: bytes) -> bytes:
     else:
         tag10142 = b'A'
     return tag10142
+
+
+def enc_string(csvdata: str) -> bytes:
+    hextmp_l = csvdata[:-1].encode().hex()
+    hextmp_r = csvdata[-1].encode().hex()
+    hextmp_r = int(hextmp_r, 16) | 0x80
+    hextmp_r = hex(hextmp_r)[2:]
+    hextmp = hextmp_l + hextmp_r
+    bytestmp = binascii.a2b_hex(hextmp)
+    return bytestmp
+
+
+def enc_int32(require: bool, csvdata: str):
+    # csvdata range detect
+    csvdata_int = int(csvdata, 10)
+    if not intrange_dict["int32"][0] <= csvdata_int <= intrange_dict["int32"][1]:
+        raise RuntimeError(f'{"int32":s} no in range {str(intrange_dict["int32"]):s}')
+    # optional and >= 0, +1
+    if not require and csvdata_int >= 0:
+        csvdata_int += 1
+    # get bin
+    if csvdata_int < 0:
+        sig = '1'
+        tmp0_int = csvdata_int * -1
+        tmp0_bin = bin(tmp0_int)[2:]
+        tmp1_bin = ''
+        for i in tmp0_bin:
+            if i == '0':
+                tmp1_bin += '1'
+            else:
+                tmp1_bin += '0'
+        tmp2_bin = tmp1_bin
+    else:
+        sig = '0'
+        tmp1_bin = bin(csvdata_int)
+        tmp2_bin = tmp1_bin[2:]
+    tmp2_bin_1blen = 1 + len(tmp2_bin)
+    tmp2_bin_7blen = math.ceil(tmp2_bin_1blen / 7.0)
+    tmp2_bin_1blenfill = (tmp2_bin_7blen)*7
+    tmp2_hex = hex(int(tmp2_bin, 2))[2:]
+    # fill 7b
+    tmp3_bin = (tmp2_bin_1blenfill - tmp2_bin_1blen) * sig + tmp2_bin
+    tmp3_bin_len = len(tmp3_bin)
+    tmp4_bin7b_list = [tmp3_bin[i*7:i*7+7] for i in range(tmp2_bin_7blen)]
+    # 7b -> 8b
+    tmp5_bin8b_list = list()
+    for index in range(len(tmp4_bin7b_list)):
+        if index == len(tmp4_bin7b_list) - 1:
+            tmp5_bin8b_list.append('1'+tmp4_bin7b_list[index])
+        else:
+            tmp5_bin8b_list.append('0'+tmp4_bin7b_list[index])
+    tmp6_hex1B_list = [hex(int(i, 2))[2:] for i in tmp5_bin8b_list]
+    tmp7_hex = ''.join(tmp6_hex1B_list)
+    tmp8_bytes = tmp7_hex.encode()
+    return tmp8_bytes
+
+
+
 
 # ================================================================================ class
 # generate step pkg(8=STEP.1.0.0 ...... .10=xxx.)
@@ -100,6 +160,7 @@ class STEP(object):
         self.stepbytes = bytes()
         for tag in stepdict.keys():
             self.stepbytes += tag + b'=' + stepdict[tag] + b'\x01'
+
 
 # generate fix pkg(8=FIX ...... .10=xxx.)
 class FIX(object):
@@ -154,6 +215,7 @@ class FIX(object):
         for tag in fixdict.keys():
             self.fixbytes += tag + b'=' + fixdict[tag] + b'\x01'
 
+
 # generate tag96 value
 class PREFETCH(object):
     def __init__(self, rd, rdlen=None, prefetchbytes=None):
@@ -167,6 +229,7 @@ class PREFETCH(object):
         else:
             self.prefetchbytes = prefetchbytes
 
+
 # split fast dicts(before encode) into step dict by tag10072
 class CSVSPLIT(object):
     def __init__(self, csvfile):
@@ -177,36 +240,121 @@ class CSVSPLIT(object):
 
     def __split__(self):
         stepsplit_dict = OrderedDict()
-        for fastdict in self.order_reader:
-            if fastdict['10072'] not in stepsplit_dict.keys():
-                stepsplit_dict[fastdict['10072']] = list()
-            stepsplit_dict[fastdict['10072']].append(fastdict)
+        for srcfastdict in self.order_reader:
+            if srcfastdict['10072'] not in stepsplit_dict.keys():
+                stepsplit_dict[srcfastdict['10072']] = list()
+            stepsplit_dict[srcfastdict['10072']].append(OrderedDict(srcfastdict))
         self.stepsplit_dict = stepsplit_dict
 
 
-"""
-fast encode rule----
-default     :
-copy        :
-increment   :
-none        :
-"""
+class GETTAG(object):
+    def __init__(self, tp, csvdata, predict):
+        self.tp = tp
+        self.require = tp[0]
+        self.op = tp[1]
+        self.dtype = tp[2]
+        self.outshow = tp[3]
+        self.csvdata = csvdata
+
+        if self.op == 'default':
+            if self.dtype == 'string':
+                if not self.require:
+                    if self.csvdata == 'Null':
+                        self.encdata = '80'
+                        self.pmapbit = 1
+                    elif self.csvdata == '':
+                        self.encdata = '0080'
+                        self.pmapbit = 1
+                    elif self.csvdata == 'default':
+                        self.encdata = None
+                        self.pmapbit = 0
+                    else:
+                        self.encdata = enc_string(self.csvdata)
+                        self.pmapbit = 1
+                else:
+                    if self.csvdata == 'Null':
+                        raise RuntimeError('default, string, 必要, 不可Null')
+                    elif self.csvdata == '':
+                        self.encdata = '80'
+                        self.pmapbit = 1
+                    elif self.csvdata == 'default':
+                        raise RuntimeError('default, string, 必要, 暂不支持default类型输入')
+                    else:
+                        # self.encdata = self.csvdata.encode().hex()
+                        self.encdata = enc_string(self.csvdata)
+                        self.pmapbit = 1
+
+            elif self.dtype == 'int32':
+                if not self.require:
+                    if self.csvdata == 'Null':
+                        self.encdata = '80'
+                        self.pmapbit = 1
+                    elif self.csvdata == '0':
+                        self.encdata = '81'
+                        self.pmapbit = 1
+                    elif self.csvdata == '':
+                        raise RuntimeError("default, int32, optional, cannot mate with ''")
+                    elif self.csvdata == 'default':
+                        self.encdata = None
+                        self.pmapbit = 0
+                    else:
+                        pass
+
+
+
+                else:
+                    pass
+
+
+            elif self.dtype == 'int64':
+                pass
+            elif self.dtype == 'uint32':
+                pass
+            elif self.dtype == 'uint64':
+                pass
+            else:
+                raise RuntimeError('wrong dtype')
+        elif self.op == 'copy':
+            pass
+        elif self.op == 'increment':
+            pass
+        elif self.op == 'none':
+            pass
+        elif self.op == 'constant':
+            pass
+        else:
+            raise RuntimeError('wrong or unsupported op')
+
 
 # encode 3201 fasts in one step
 class ENCODE3201(object):
-    def __init__(self, fasts_list):
-        self.fasts_list = fasts_list
+    PREDICT = dict()
 
+    def __init__(self, srcfasts_list):
+        self.template = tpdict[3201]
+        self.srcfasts_list = srcfasts_list
+        self.__encode_many__()
 
-
-
-
-
+    def __encode_many__(self):
+        encfastdict = OrderedDict()
+        for srcfastdict in self.srcfasts_list:
+            for tag in srcfastdict.keys():
+                if tag in ['10072', '35', ]:
+                    pass
+                elif tag == '999':  # ('Y', 'copy', 'int32', False, 'nrepeat')
+                    csvdata = srcfastdict['999']
+                    tp = self.template['00999']
+                    gt = GETTAG(tp, csvdata, ENCODE3201.PREDICT)
 
 
 if __name__ == "__main__":
+    enc_string('600613')
+    enc_int32(False, '942755')
+    enc_int32(True, '942755')
+    enc_int32(False, '-942755')
+    enc_int32(True, '-7942755')
     tp3201_dict = tplist2dict(template.template_3201)
     mo3201 = CSVSPLIT("./order3201.csv")
-    for fasts_list in mo3201.stepsplit_dict.values():
-        o3201 = ENCODE3201(fasts_list)
+    for srcfasts_list in mo3201.stepsplit_dict.values():
+        o3201 = ENCODE3201(srcfasts_list)
         print('debug')
